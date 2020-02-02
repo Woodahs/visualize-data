f8893a0ef334278e8c52d78089d9720cea8ff580
Wojciech Dynus <wojciech.dynus@trycoffeemug.net>
2020-02-02 20:37:01 +0100

    Updates

---
+++ precache-manifest.63f512b16e3348e1dd0a84cbc965862e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d33ed69da70bb0bb274d16d72304d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3f4380f630957ec92db",
+    "url": "/static/css/main.58eee130.chunk.css"
+  },
+  {
+    "revision": "2a9dd71c3100593e0860",
+    "url": "/static/js/2.906997d8.chunk.js"
+  },
+  {
+    "revision": "0362bb6d736d80bdb3d9581cd3f6e66a",
+    "url": "/static/js/2.906997d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3f4380f630957ec92db",
+    "url": "/static/js/main.b7cb36ec.chunk.js"
+  },
+  {
+    "revision": "9dc4a7910e99d4b0bd78",
+    "url": "/static/js/runtime-main.bb4d1fb7.js"
+  }
+]);
